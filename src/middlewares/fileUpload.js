1ca4a22676c6fc6454eee76cd7d98b65ab590405
ashish280601 <119057692+ashish280601@users.noreply.github.com>
2024-04-03 21:50:24 +0530

    added feature to extract the extension from img in multer

--- src/middlewares/fileUpload.js
+++ src/middlewares/fileUpload.js
@@ -10,16 +10,25 @@ Step to create a multer
 import multer from "multer";
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, './uploads/');
-    },
-    filename: (req, file, cb) => {
-        cb(null, new Date().toISOString() + file.originalname);
-    }
-})
+  destination: (req, file, cb) => {
+    cb(null, "./uploads/");
+  },
+  filename: (req, file, cb) => {
+    // extracting file extension
+    const ext = file.originalname.split(".").pop();
+    // construct aa unique filename with a timestamp
+    const filename =
+      new Date().toISOString().replace(/:/g, "-") +
+      "-" +
+      Math.random().toString(36).substring(7) +
+      "-" +
+      ext;
+    cb(null, filename);
+  },
+});
 
 const uploads = multer({
-    storage: storage
+  storage: storage,
 });
 
 export default uploads;
