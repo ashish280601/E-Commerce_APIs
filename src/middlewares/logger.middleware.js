cf2944d8006bdc91342582f97487bf6aeff1c655
ashish280601 <119057692+ashish280601@users.noreply.github.com>
2024-04-19 22:11:34 +0530

    Loggeing using winston library

--- src/middlewares/logger.middleware.js
+++ src/middlewares/logger.middleware.js
@@ -3,10 +3,12 @@ Logger middleware is used to log user request so it can help to find an error in
 */
 // 1. Import file system
 import fs from "fs";
+import winston, { transports } from "winston";
 
 const fsPromise = fs.promises;
 
 // 2. Create a function file to store a log every request of an client.
+// Method 1: Using File System
 async function log(logData) {
   try {
     logData = `\n ${new Date().toString()} - ${logData}`;
@@ -16,12 +18,23 @@ async function log(logData) {
   }
 }
 
+// Method 2: Using an winston library
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'request-logging' },
+  transports: [
+    new winston.transports.File({filename: 'log.txt'})
+  ]
+})
+
 // creating a middleware function
 const loggerMiddleware = async (req, res, next) => {
   // 1. Log request body
   if (!req.url.includes("signin")) {
     const logData = `${req.url} - ${JSON.stringify(req.body)}`;
-    await log(logData);
+    // await log(logData);
+    logger.info(logData);
   }
   next();
 };   
