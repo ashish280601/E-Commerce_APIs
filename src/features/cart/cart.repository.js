a9853b435692a6c5669fbd74177aabd65589cf01
ashish280601 <119057692+ashish280601@users.noreply.github.com>
2024-04-28 01:11:59 +0530

    setup an repository file foor cart

---
+++ src/features/cart/cart.repository.js
@@ -0,0 +1,17 @@
+export default class CartRepository{
+    constructor(){
+        this.collection = "cart"
+    }
+
+    async getCart(){
+        // write your code logic here
+    }
+
+    async addCart(){
+        // write your code logic here
+    }
+
+    async delCart(){
+        // write your code logic here
+    }
+}
