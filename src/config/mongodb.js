3e2a898ceb3575883ea6b82e46b9756da97807f9
ashish280601 <119057692+ashish280601@users.noreply.github.com>
2024-05-03 22:02:56 +0530

    creating indexes

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -14,6 +14,7 @@ const connectToMongoDB = () => {
             client = clientInstance
             console.log("MongoDB is connected");
             createCounter(client.db("e-CommerceDB"));
+            createIndexes(client.db("e-CommerceDB"));
         })
         .catch(err => {
             console.log(err);
@@ -28,6 +29,23 @@ const createCounter = async(db) => {
     const exisitingCounter = await db.collection("counters").findOne({ _id: "cartItemID"});
     if(!exisitingCounter){
         await db.collection("counters").insertOne({ _id: "cartItemID", value: 0});
+    }
+}
+
+// creating an indexes
+const createIndexes = async(db) => {
+    try {
+        // single field indexes
+        await db.collection("products").createIndex({ price: 1 })
+        // compound field indexes
+        await db.collection("products").createIndex({ name:1, cateogry: -1})
+        // text indexes
+        await db.collection("products").createIndex({ desc: "text" });
+
+
+        console.log("Indexes Created");
+    } catch (error) {
+        console.log(error);
     }
 }
 
