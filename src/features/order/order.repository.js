c878bcff410056c7d0f5687f0d7f8ad64a0d2ba0
ashish280601 <119057692+ashish280601@users.noreply.github.com>
2024-05-05 23:56:00 +0530

    calculated the totalAmount price of an total product in cartItem in Transaction part 2

--- src/features/order/order.repository.js
+++ src/features/order/order.repository.js
@@ -1,17 +1,57 @@
+import { ObjectId } from "mongodb";
+import { getDB } from "../../config/mongodb.js";
+
 export default class OrderRepository{
     constructor(){
         this.collection = "orders";
     }
 
-    async placeOrder( userID ){
+    async placeOrder( userId ){
         // write your code logic here.
         
         // 1. Get cartItem and calculate totoal amount.
+        await this.getTotalAmount(userId)
 
         // 2. Create an order record.
 
         // 3. Reduce the stock 
 
         // 4. Clear the cart items.
+    }
+
+    async getTotalAmount(userId){
+        const db = getDB();
+
+        const totalAmount = await db.collection("cart").aggregate([
+            // Step 1. Match the userID to find the total cart product of that users
+            {
+                $match: { userID: new ObjectId(userId)}
+            },
+            // Step 2. Get the user product details from product collection.
+            {
+                $lookup:{
+                    from: "products",
+                    localField: "productID",
+                    foreignField: "_id",
+                    as: "productInfo"
+                }
+            },
+            // Step 3. Descontructing the nested object array into an document fields
+            {
+
+                $unwind: "$productInfo"
+            },
+            // Step 4. Calculate the productItem and quantity
+            {
+                $addFields:{
+                    "totalAmount": {
+                        $multiply: ["$productInfo.price", "$quantity"]
+                    }
+                }
+            }
+
+        ]).toArray();
+        const finalTotalAmount = totalAmount.reduce((amount, productItem) => amount + productItem.totalAmount, 0)
+        console.log("finalTotalAmount", finalTotalAmount);    
     }
 }
